cf5d237932fdd0f06c4c284f4030cc72a27cab7c
m0001123 <m220131133@gmail.com>
2023-07-10 17:14:19 +0800

    flutterUi練習

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:bookticket/Page/bottom_bar.dart';
+import 'package:bookticket/style.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+       primaryColor: Style.primaryColor,
+        useMaterial3: true,
+      ),
+      home: BottomBar(),
+    );
+  }
+}
+
+
